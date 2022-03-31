34a05e8737a0894d5d24f1386c1cca43aacc06a6
DominikTher <dominik.ther@windowslive.com>
2022-03-31 23:43:33 +0200

    Added ProductControllerTests

---
+++ tests/eShop.Api.Unit.Tests/Controllers/ProductControllerTests.cs
@@ -0,0 +1,77 @@
+﻿using eShop.Api.Controllers;
+using eShop.Application.Features.Products;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace eShop.Api.Unit.Tests.Controllers;
+
+[TestFixture]
+public class ProductControllerTests
+{
+    private IMediator mediator = default!;
+    private ProductController sut = default!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        mediator = Substitute.For<IMediator>();
+        sut = new ProductController(mediator);
+    }
+
+    [Test]
+    public async Task GetProducts_ReturnsExpectedType()
+    {
+        // Arrange
+        var cancellationToken = new CancellationToken();
+
+        // Act
+        var response = await sut.GetProducts(cancellationToken);
+
+        // Assert
+        response
+            .Should()
+            .BeOfType<ActionResult<IEnumerable<ProductOutput>>>();
+    }
+
+    [Test]
+    public async Task GetProducts_ReturnsExpectedResult()
+    {
+        // Arrange
+        var cancellationToken = new CancellationToken();
+        mediator
+            .Send(Arg.Any<ProductsRequest>(), cancellationToken)
+            .Returns(new List<ProductOutput> {
+                new() {
+                    Id = 1,
+                    Description = "fake",
+                    ImgUri = "uri",
+                    Name = "name",
+                    Price = 99
+                }
+            });
+        var expected = new OkObjectResult(new List<ProductOutput> {
+            new() {
+                Id = 1,
+                Description = "fake",
+                ImgUri = "uri",
+                Name = "name",
+                Price = 99
+            }
+        });
+
+        // Act
+        var response = await sut.GetProducts(cancellationToken);
+
+        // Assert
+        response
+            .Result
+            .Should()
+            .BeEquivalentTo(expected);
+    }
+}
